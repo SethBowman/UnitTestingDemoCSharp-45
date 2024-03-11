5efca886ab1d635138225d5e6327a09d0cba30f7
Seth Bowman <brandonsethbowman@gmail.com>
2024-03-11 14:11:52 -0500

    Add project files.

---
+++ MainProject/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject
+{
+    public class Helper
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public string Greeting(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return "Hello!";
+            }
+
+            return $"Hello, {name}!";
+            
+        }
+    }
+}
